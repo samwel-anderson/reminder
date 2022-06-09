e24d13edcb7a3da77dc3061daae457d4a3b14c82
jenadiusnicholaus <ucode888@gmail.com>
2022-06-09 12:01:03 +0300

    add seach,modified booking

--- lodge_app/urls.py
+++ lodge_app/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
@@ -8,7 +9,8 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', views.index, name='home'),
     path('room_details/<int:pk>', views.RoomDetails.as_view() ,name= 'room_details' ),
-    path('manageBookingstatus', views.manageBookingStatus, name= 'manageBookingstatus' )
+    path('manageBookingstatus', views.manageBookingStatus, name= 'manageBookingstatus' ),
+    path('search-reservation', views.searchReservation, name= "search-reservation")
 
     # path('', RedirectView.as_view(url="/")),
     # path('index/',views.index, name = "index"),
